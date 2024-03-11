2f732f3b3940f502f6faa6c60d97962fe2630a1c
Shahrukh <shahrukh1494@gmail.com>
2024-03-11 15:45:30 +0530

    basic routing

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,7 @@
-'use client'
-import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
-  const [name, setName] = useState("Anil")
-
-  const apple = () => {
-    setName("Sidhu")
-  }
-
-  const InnerComp = () => {
-    return <h1>Inner Component</h1>
-  }
-
   return (
-    <main className={styles.main}>
-      <h1>Events, functions, and state - {name}</h1>
-      <button onClick={apple}>Click me</button>
-      
-      {/* Called as a React component */}
-      <InnerComp />
-      
-      {/* Called as a function */}
-      {InnerComp()}
-    </main>
-  );
+    <h1>Home Page</h1>
+  )
 }
