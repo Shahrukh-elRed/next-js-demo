650c2d923b8990999c6e00da7218514c9883f153
Shahrukh <shahrukh1494@gmail.com>
2024-03-11 13:35:33 +0530

    events, functions and state

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,28 @@
+'use client'
+import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [name, setName] = useState("Anil")
+
+  const apple = () => {
+    setName("Sidhu")
+  }
+
+  const InnerComp = () => {
+    return <h1>Inner Component</h1>
+  }
+
   return (
     <main className={styles.main}>
-      <h1>Home</h1>
+      <h1>Events, functions, and state - {name}</h1>
+      <button onClick={apple}>Click me</button>
+      
+      {/* Called as a React component */}
+      <InnerComp />
+      
+      {/* Called as a function */}
+      {InnerComp()}
     </main>
   );
 }
