37d6f47df29945791dcb5be4827909c552775837
Shahrukh <shahrukh1494@gmail.com>
2024-03-11 16:49:41 +0530

    nested routing

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,10 +1,22 @@
+"use client"
 import Link from "next/link"
+import { useRouter } from "next/navigation";
 
 const Login = () => {
+    const router = useRouter();
+    const navigate = (path) => {
+        router.push("/login/" + path)
+    }
   return (
       <div>
         <h1>Login Page</h1>
         <Link href="/">Go to Home Page</Link>
+        <br />
+        <br />
+        <button onClick={() => navigate("loginteacher")}>Got to Login Teacher Page</button>
+        <br />
+        <br />
+        <button onClick={() => navigate("loginstudent")}>Got to Login Student Page</button>
       </div>
   )
 }
