cc30a714a1a0398ca2b6ae9078914d866ac5f5de
Shahrukh <shahrukh1494@gmail.com>
2024-03-12 18:01:49 +0530

    conditional styles

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,11 @@
 import Link from "next/link";
 import style1 from "@/other.module.css";
 import style2 from "@/custom.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [color, setColor] = useState("red")
+  const { red } = style2;
 
   return (
     <main>
@@ -19,6 +22,13 @@ export default function Home() {
       <Link href="/user">Go to Global and inline styles component</Link>
       <div className={style1.main}>Custom CSS dark blue color</div>
       <div className={style2.main}>Custom CSS dark gray color</div>
+      <div className={color === "red" ? style2.red : style2.green}>Condition with style</div>
+      <button onClick={() => setColor(color === "red" ? "green" : "red")}>Update Color</button>
+      <div id={style2.whitetext} style={color === "red" ? {background: "red"} : {background: "green"}}>Condition with Inline style</div>
+      <div className={red}>Red 1</div>
+      <div className={red}>Red 2</div>
+      <div className={red}>Red 3</div>
+      <div className={red}>Red 4</div>
     </main>
   )
 }
