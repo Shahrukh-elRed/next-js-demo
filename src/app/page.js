85500142a18d77ad4fd149b2ba773570c2205a52
Shahrukh <shahrukh1494@gmail.com>
2024-03-12 19:01:40 +0530

    foint optimization

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
-import styles from "./page.module.css";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Home</h1>
+    <main>
+      {/* <h1 style={{ fontFamily: "Roboto"}}>Font optimization in Next JS</h1>
+      <h1 style={{ fontFamily: "Madimi One", fontWeight: "200"}}>Font family in Next JS</h1> */}
+      <h1 className={roboto.className}>Font optimization in Next JS</h1>
+      <h1 className={roboto.className}>Font family in Next JS</h1>
     </main>
   );
 }
