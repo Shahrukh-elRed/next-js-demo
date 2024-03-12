cfd0f3ccd3d37f195a23602c3c0109cdc350f9f2
Shahrukh <shahrukh1494@gmail.com>
2024-03-12 13:35:41 +0530

    fetch API in client component

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,12 @@
 "use client"
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-  const navigate = (name) => {
-    router.push(name);
-  }
 
   return (
     <main>
-      <h1>Basic Routing | Make New Page</h1>
-      <Link href="/login">Go to Login Page</Link>
-      <br />
-      <br />
-      <Link href="/about">Go to About Page</Link>
-      <br />
-      <br />
-      <button onClick={() => navigate("/login")}>Go to Login Page</button>
-      <br />
-      <br />
-      <button onClick={() => navigate("/about")}>Go to About Page</button>
+      <h1>fetch data with API in Client Component</h1>
+      <Link href="/productlist">Go to Products List</Link>
     </main>
   )
 }
---
+++ src/app/productlist/page.js
@@ -0,0 +1,28 @@
+"use client"
+import { useEffect, useState } from "react"
+
+const ProductList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            let data = await fetch("https://dummyjson.com/products");
+            data = await data.json();
+            setProducts(data.products);
+        }
+        fetchData();
+    }, [])
+
+    return (
+        <div>
+            <h1>Product List</h1>
+            {
+                products.map((item) => 
+                    <h3 key={item.id}>Name: {item.title}, Price: {item.price}</h3>
+                )
+            }
+        </div>
+    )
+}
+
+export default ProductList
