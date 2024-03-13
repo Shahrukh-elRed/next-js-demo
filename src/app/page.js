f62dc05276875bad0d4c30b71fa7fb5cbce56b0d
Shahrukh <shahrukh1494@gmail.com>
2024-03-13 17:11:11 +0530

    use of environment variables and constants

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,20 @@
+import { API_BASE_URL } from "@/config/constants";
+
 export default function Home() {
+  console.log(process.env.SERVER_PASSWORD)
+  console.log(process.env.DB_PASSWORD)
+  
+
   return (
     <main>
-      <h1>Home</h1>
+      {
+        process.env.NODE_ENV === "development" ?
+        <h2>We are on Development mode</h2>
+        : 
+        <h2>We are on Production mode</h2>
+      }
+      <h1>Environment Variables in Next JS</h1>
+      <h2>API base URL : {API_BASE_URL}</h2>
     </main>
   );
 }
