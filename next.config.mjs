994be88b1b5eaa1ae09837e659f95e80f23b7e71
Shahrukh <shahrukh1494@gmail.com>
2024-03-13 15:59:38 +0530

    redirect using nextConfig for non existing page also

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,11 @@ const nextConfig = {
                 destination: "/",
                 permanent: false,
             },
+            {
+                source: "/college",
+                destination: "/",
+                permanent: false,
+            },
         ]
     }
 };
