8c1dae786bcb2dc7ee3b3cd6a2578c858c447bcd
Shahrukh <shahrukh1494@gmail.com>
2024-03-13 09:45:40 +0530

    dynamic meta data using generateMetadata function

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Home</h1>
+      <h1>generateMetadata in Next</h1>
+      <Link href="/user">User Page</Link>
     </main>
   );
+}
+
+export const generateMetadata = () => {
+  return {
+      title: "student page title",
+      description: "student page description",
+  }
 }
---
+++ src/app/user/page.js
@@ -0,0 +1,16 @@
+const User = () => {
+  return (
+    <div>
+        <h1>User</h1>
+    </div>
+  )
+}
+
+export default User
+
+export const generateMetadata = () => {
+    return {
+        title: "user page title",
+        description: "user page description",
+    }
+}
