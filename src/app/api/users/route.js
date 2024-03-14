351e0861f0f061d986fee212e9bc8e6b775b32ef
Shahrukh <shahrukh1494@gmail.com>
2024-03-14 10:41:53 +0530

    post api with post method

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -4,3 +4,12 @@ import { NextResponse } from "next/server"
 export const GET = () => {
     return NextResponse.json(users, { status: 200 })
 }
+
+export const POST = async (request) => {
+    let payload = await request.json()
+    console.log(payload)
+    if (!payload.name || !payload.age || !payload.email) {
+        return NextResponse.json({ result: "Required field not found", success: false }, { status: 400 })
+    }
+    return NextResponse.json({ result: "New user created", success: true }, { status: 201 })
+}
