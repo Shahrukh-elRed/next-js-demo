12285560cb99af7c6975925508236c83f4d976be
Shahrukh <shahrukh1494@gmail.com>
2024-03-15 13:48:51 +0530

    next js deployment

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="text-center">
+      <h1>Next JS Deployment</h1>
+      <h2>
+        This is just an example of Next JS site deployment
+      </h2>
     </main>
   );
 }
